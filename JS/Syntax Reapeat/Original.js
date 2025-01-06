07619d937d56d9e757dfb14a34f4b8985fad73c0
Keko <kekonline@gmail.com>
2025-01-06 08:21:37 +0100

    more syntax repeat

--- JS/Syntax Reapeat/Original.js
+++ JS/Syntax Reapeat/Original.js
@@ -1,10 +1,21 @@
 const var1 = () => { };
+const var1 = () => { };
+const var1 = () => { };
+
 const var2 = () => a;
+const var2 = () => a;
+const var2 = () => a;
+
 const add1 = (a, b) => a + b;
 (() => { console.log("Hello") })()
 const var3 = async () => { };
 
 var1.forEach((ele) => { console.log("hi") });
+var1.forEach((ele) => { console.log("hi") });
+var1.forEach((ele) => { console.log("hi") });
+
+var1.forEach(ele => console.log("we"));
+var1.forEach(ele => console.log("we"));
 var1.forEach(ele => console.log("we"));
 
 var1.map(ele => ele * 2);
@@ -24,42 +35,6 @@ var1.flatMap(element => [element, element * 2]);
 var1.flatMap((element) => {
     return [element, element * 2];
 });
-
-isLoggedIn && showWelcomeMessage();
-isLoggedIn || showWelcomeMessage();
-
-let name2 = inputName ?? "Guest";
-let name5 = inputName || "Guest";
-
-let fetchData = data && fetchDataFromAPI();
-let message = user && `Hello, ${user.name}`;
-
-// SET 1
-
-const var1 = () => { };
-const var1 = () => { };
-const var1 = () => { };
-
-const var1 = () => 1;
-const var1 = () => a;
-const var1 = () => a;
-
-const var1 = async () => { };
-const var1 = async () => { };
-const var1 = async () => { };
-
-var1.forEach((ele) => { ele })
-var1.forEach((ele) => { ele })
-var1.forEach((ele) => { ele })
-
-var1.forEach(ele => ele)
-var1.forEach(ele => ele)
-var1.forEach(ele => ele)
-
-var1.reduce((accumulator, current) => accumulator + current, 0);
-var1.sort((a, b) => a - b);
-var1.map(ele => ele * 2);
-var1.flatMap(element => [element, element * 2]);
 
 isLoggedIn && showWelcomeMessage();
 isLoggedIn || showWelcomeMessage();
---
+++ JS/Syntax Reapeat/Set1.js
@@ -0,0 +1,46 @@
+const var1 = () => { };
+const var1 = () => { };
+const var1 = () => { };
+
+const var2 = () => a;
+const var2 = () => a;
+const var2 = () => a;
+
+const add1 = (a, b) => a + b;
+(() => { console.log("Hello") })()
+const var3 = async () => { };
+
+var1.forEach((ele) => { console.log("hi") });
+var1.forEach((ele) => { console.log("hi") });
+var1.forEach((ele) => { console.log("hi") });
+
+var1.forEach(ele => console.log("we"));
+var1.forEach(ele => console.log("we"));
+var1.forEach(ele => console.log("we"));
+
+var1.map(ele => ele * 2);
+var1.map((ele) => { return ele * 2 })
+
+var1.reduce((accumulator, current) => accumulator + current, 0);
+var1.reduce((accumulator, current) => {
+    return accumulator + current;
+}, 0);
+
+var1.sort((a, b) => a - b);
+var1.sort((a, b) => {
+    return a - b;
+});
+
+var1.flatMap(element => [element, element * 2]);
+var1.flatMap((element) => {
+    return [element, element * 2];
+});
+
+isLoggedIn && showWelcomeMessage();
+isLoggedIn || showWelcomeMessage();
+
+let name2 = inputName ?? "Guest";
+let name5 = inputName || "Guest";
+
+let fetchData = data && fetchDataFromAPI();
+let message = user && `Hello, ${user.name}`;
