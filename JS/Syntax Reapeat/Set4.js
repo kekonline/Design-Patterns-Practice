d0d8f00de1743b007cf3420aff6b6188eaa0fcec
Keko <kekonline@gmail.com>
2025-01-09 21:35:57 +0100

    more syntax practice

---
+++ JS/Syntax Reapeat/Set4.js
@@ -0,0 +1,11 @@
+const var1 = () => { };
+const var2 = () => a;
+
+const add = (a, b) => a + b;
+
+
+(() => { console.log("Hi") })()
+
+var1.forEach((ele) => { consolelog("hi") });
+
+var2.map(ele => ele * 2)
